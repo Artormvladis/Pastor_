9d22f0428c604cbb65878ebb09053e7d438a4729
Артём <112126702+Artormvladis@users.noreply.github.com>
2025-06-03 20:21:48 +0300

    Pastor

--- Pastor/Assets/Scripts/Knopkavnutri.cs
+++ Pastor/Assets/Scripts/Knopkavnutri.cs
@@ -1,17 +1,27 @@
+using Newtonsoft.Json.Bson;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Knopkavnutri : MonoBehaviour
 {
     [SerializeField] GameObject _canold;
     [SerializeField] GameObject _door1;
     [SerializeField] GameObject _door2;
+    [SerializeField] string level;
     public static bool isPress1 = false;
-    private void OnTriggerEnter(Collider other)
+    public static string levelchange = "";
+    private async void OnTriggerEnter(Collider other)
     {
+        levelchange = level;
         if (isPress1) return;
         isPress1 = true;
         _canold.transform.position += new Vector3(-0.02f, 0, 0);
-        _door1.transform.position += new Vector3(-1, 0, 0);
-        _door2.transform.position += new Vector3(1, 0, 0);
+        _door1.transform.position += new Vector3(1, 0, 0);
+        _door2.transform.position += new Vector3(-1, 0, 0);
+        Invoke("Levelchange", 2f);
+    }
+    private void Levelchange(string levelchange)
+    {
+        SceneManager.LoadScene("level " + levelchange);
     }
 }
